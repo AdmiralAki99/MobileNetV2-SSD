f9e0086a25448c5395756136c743d84b80ced1de
Akhilesh Warty <akilesshw@gmail.com>
2026-07-17 22:01:48 -0400

    Added Configs and Updated .gitignore clearing the UI for the dashboard

--- ui/tests/setupTests.ts
+++
@@ -1,6 +0,0 @@
-const originalError = console.error.bind(console)
-console.error = (...args: unknown[]) => {
-  const msg = typeof args[0] === 'string' ? args[0] : ''
-  if (msg.includes('not wrapped in act(')) return
-  originalError(...args)
-}
